4b6af4b316bf92c8957ee0c5d55ae63218fc553a
FlavioLionelRita <flaviolrital@hotmail.com>
2022-10-22 01:55:54 +0200

    expose Cache

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,8 @@
 import { H3lp } from './helpers/h3lp'
-export * from './helpers'
+
 export * from './contract/delta'
 export * from './contract/base'
 export * from './contract/test'
+export * from './core/cache'
+export * from './helpers'
 export const h3lp = new H3lp()
