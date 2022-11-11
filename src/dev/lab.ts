be62d2520ee05e7db99d734c78fd00abdc899977
FlavioLionelRita <flaviolrital@hotmail.com>
2022-11-11 18:20:32 +0100

    remove variable unusued

--- src/dev/lab.ts
+++ src/dev/lab.ts
@@ -1,11 +1,6 @@
 /* eslint-disable no-template-curly-in-string */
 import { h3lp } from '../lib'
 ;(async () => {
-	const context = {
-		firstName: 'Pedro',
-		lastName: 'Brieger',
-		portal: 'www.nodal.am'
-	}
 	let result = h3lp.utils.template('Display: ${DISPLAY} shell: ${SHELL}', h3lp.utils.createEnvironmentVariableReplacer())
 	console.log(result)
 	result = h3lp.utils.template('Display: $DISPLAY shell: $SHELL', h3lp.utils.createEnvironmentVariableReplacer())
